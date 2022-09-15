68b985d9aa0041093190e956311c7f1bf63f121c
kaiwen zhang <kaiwen.zhang@carta.com>
2022-09-15 14:48:17 -0700

    durable tx compiles

--- components/HomeView.tsx
+++ components/HomeView.tsx
@@ -1,15 +1,12 @@
 import styles from '../styles/Home.module.css'
 import React, { useEffect, useState } from 'react';
 import { useWallet, useConnection, WalletContextState } from '@solana/wallet-adapter-react';
-import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
+import { Connection, Keypair, LAMPORTS_PER_SOL, NonceAccount, NONCE_ACCOUNT_LENGTH, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
 import { getUserSOLBalance, transfer } from '../utils/helpers';
 import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
-import { sign } from 'crypto';
 
 const destAddress = new PublicKey('2oaH7AKTzzT2LrXKhAzqeXFPvSTpj2hrksXXGg5dSXFz')
 const transferAmount = 0.1 * LAMPORTS_PER_SOL;
-const NonceAccountSize = 80;
-
 
 const HomeView = () => {
     const wallet = useWallet();
@@ -26,17 +23,15 @@ const HomeView = () => {
     const createAndInitializeNonceAccount = async (wallet: WalletContextState, connection: Connection) => {
         if (!wallet.publicKey) throw new WalletNotConnectedError();
         const nonceAccount = Keypair.generate();
-        const nonceAccountRentFreeBalance = await connection.getMinimumBalanceForRentExemption(NonceAccountSize)
-        
-        const latestBlockhash = await connection.getLatestBlockhash();
-        console.log('before sending tx')
+        const nonceAccountRentFreeBalance = await connection.getMinimumBalanceForRentExemption(NONCE_ACCOUNT_LENGTH)
 
-        const transaction = new Transaction({...latestBlockhash, feePayer: wallet.publicKey}).add(
-            SystemProgram.createNonceAccount({
-                authorizedPubkey: wallet.publicKey,
+        const tx = new Transaction().add(
+            SystemProgram.createAccount({
                 fromPubkey: wallet.publicKey,
+                newAccountPubkey: nonceAccount.publicKey,
                 lamports: nonceAccountRentFreeBalance,
-                noncePubkey: nonceAccount.publicKey
+                space: NONCE_ACCOUNT_LENGTH,
+                programId: SystemProgram.programId
              }),
              SystemProgram.nonceInitialize({
                 noncePubkey: nonceAccount.publicKey,
@@ -44,38 +39,111 @@ const HomeView = () => {
              })
         );
 
-        transaction.partialSign(nonceAccount)
-        const a = String.fromCharCode.apply(null, transaction.serialize({requireAllSignatures: false}) as any)
-        console.log("~~~~", btoa(a) )
-
-        const signature = await wallet.sendTransaction(transaction, connection, {  signers: [nonceAccount], skipPreflight: true });
-        console.log('after sending tx: ', signature)
-        // await connection.confirmTransaction({ blockhash, lastValidBlockHeight, signature });
-        console.log("transaction confirmed")
+        const signature = await wallet.sendTransaction(tx, connection,  {  signers: [nonceAccount] })
+        console.log('nonce account initialized: ', signature)
         return nonceAccount
+
+        // let tx = new Transaction().add(
+        //     // create nonce account
+        //     SystemProgram.createAccount({
+        //       fromPubkey: wallet.publicKey,
+        //       newAccountPubkey: nonceAccount.publicKey,
+        //       lamports: await connection.getMinimumBalanceForRentExemption(
+        //         NONCE_ACCOUNT_LENGTH
+        //       ),
+        //       space: NONCE_ACCOUNT_LENGTH,
+        //       programId: SystemProgram.programId,
+        //     }),
+        //     // init nonce account
+        //     SystemProgram.nonceInitialize({
+        //       noncePubkey: nonceAccount.publicKey, // nonce account pubkey
+        //       authorizedPubkey: wallet.publicKey, // nonce account authority (for advance and close)
+        //     })
+        // );
+
+        // const signature = await wallet.sendTransaction(tx, connection,  {  signers: [nonceAccount] })
+        // console.log('nonce account initialized: ', signature)
+
+        // const nonceAccountRentFreeBalance = await connection.getMinimumBalanceForRentExemption(NonceAccountSize)
+        
+        // const latestBlockhash = await connection.getLatestBlockhash();
+        // console.log('before sending tx')
+
+        // const transaction = new Transaction({...latestBlockhash, feePayer: wallet.publicKey}).add(
+        //     SystemProgram.createNonceAccount({
+        //         authorizedPubkey: wallet.publicKey,
+        //         fromPubkey: wallet.publicKey,
+        //         lamports: nonceAccountRentFreeBalance,
+        //         noncePubkey: nonceAccount.publicKey
+        //      }),
+        //      SystemProgram.nonceInitialize({
+        //         noncePubkey: nonceAccount.publicKey,
+        //         authorizedPubkey: wallet.publicKey
+        //      })
+        // );
+
+        // transaction.partialSign(nonceAccount)
+        // const a = String.fromCharCode.apply(null, transaction.serialize({requireAllSignatures: false}) as any)
+        // console.log("~~~~", btoa(a) )
+
+        // const signature = await wallet.sendTransaction(transaction, connection, {  signers: [nonceAccount], skipPreflight: true });
+        // console.log('after sending tx: ', signature)
+        // // await connection.confirmTransaction({ blockhash, lastValidBlockHeight, signature });
+        // console.log("transaction confirmed")
+        // return nonceAccount
     }
 
     const createDurableTx = async (wallet: WalletContextState, connection: Connection, to: PublicKey, amount: number) => {
         if (!wallet.publicKey) throw new WalletNotConnectedError();
 
-        const nonceAccount = await createAndInitializeNonceAccount(wallet, connection);
-        const na = await connection.getNonce();
-        na.
-        const transaction = new Transaction({nonceInfo: {
-            nonce: ...,
-            nonceInstruction: SystemProgram.nonceAdvance({noncePubkey: nonceAccount.publicKey, authorizedPubkey: wallet.publicKey}),
-        }}).add(
-            
+        const nonceAccountKeypair  = await createAndInitializeNonceAccount(wallet, connection);
+        const accountInfo = await connection.getAccountInfo(nonceAccountKeypair.publicKey);
+        console.log('accountInfo: ', accountInfo)
+        const nonceAccount = accountInfo && NonceAccount.fromAccountData(accountInfo.data);
+        console.log('nonceAccount: ', nonceAccount)
+
+        let tx = new Transaction().add(
+            // nonce advance must be the first insturction
+            SystemProgram.nonceAdvance({
+              noncePubkey: nonceAccountKeypair.publicKey,
+              authorizedPubkey: wallet.publicKey,
+            }),
+            // after that, you do what you really want to do, here we append a transfer instruction as an example.
             SystemProgram.transfer({
-                fromPubkey: wallet.publicKey,
-                toPubkey: to,
-                lamports: amount,
-              })
-          );
+              fromPubkey: wallet.publicKey,
+              toPubkey: destAddress,
+              lamports: amount,
+            })
+        );
 
-        const signedTransaction = wallet.signTransaction && await wallet.signTransaction(transaction)
+        // assign `nonce` as recentBlockhash
+        if (nonceAccount && nonceAccount.nonce){
+            tx.recentBlockhash = nonceAccount.nonce
+        } else {
+            return "nonce not found";
+        }   
+        
+        tx.feePayer = wallet.publicKey;
+        tx.sign(
+            nonceAccountKeypair
+        ); /* fee payer + nonce account authority + ... */
 
-        setDurableTx(signedTransaction)
+        setDurableTx(tx);
+        console.log('durable tx buffer: ', Buffer.from(JSON.stringify(tx)));
+
+        // const na = await connection.getNonce();
+        // na.
+        // const transaction = new Transaction({nonceInfo: {
+        //     nonce: ...,
+        //     nonceInstruction: SystemProgram.nonceAdvance({noncePubkey: nonceAccount.publicKey, authorizedPubkey: wallet.publicKey}),
+        // }}).add(
+            
+        //     SystemProgram.transfer({
+        //         fromPubkey: wallet.publicKey,
+        //         toPubkey: to,
+        //         lamports: amount,
+        //       })
+        //   );
     }
 
     return (
